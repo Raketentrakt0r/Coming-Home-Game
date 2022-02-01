a35deced7d89651bad31e7136c551073f2b691c0
Luis Moncada <raketentrakt0r@gmail.com>
2022-02-01 12:10:10 +0100

    switch-case to move, not finisched

--- scripts/player.js
+++ scripts/player.js
@@ -4,8 +4,12 @@ class Player{
         this.width = 35;
         this.height = 25;
         
-        this.x = -10;
-        this.y = -10;
+        this.x = 10;
+        this.y = 10;
+
+        this.vx = 0;
+        this.vy = 0;
+        
 
         this.img = new Image();
         this.img.src = "/images/rocket.png"; 
@@ -13,9 +17,38 @@ class Player{
 
     init(){
         this.x= 10;
-        this.y= 10;  
+        this.y= 10;
+        this.vx = 0;
+
     }
 
+    move(){
+        document.addEventListener("keydown", (e) =>{
+            switch(e.keyCode){
+                case 38:
+                    player.vy -= 1;
+                    break;
+                case 40:
+                    player.vy += 1;
+                    break;
+                case 37:
+                    player.vx -= 1;
+                    break;
+                case 39:
+                    player.vx += 1;
+                    break;
+            }
+        });
+
+        document.addEventListener("keyup",(e) =>{
+            player.vx=0;
+            player.vy=0;
+        })
+
+
+    }
+    
+    
     draw(frameNumber){
         console.log("Qué pasa!?")
         this.ctx.drawImage(
@@ -23,7 +56,8 @@ class Player{
         this.x,
         this.y,
         this.width,
-        this.height
+        this.height,
+        
         );
     };
 }
